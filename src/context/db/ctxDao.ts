d79d8ab95c9f87e075e2d6fbdf062bdf54dbb61e
ferbragon <nando.gon17@gmail.com>
2024-04-24 17:06:49 -0600

    feat(src/context/db/ctxDao.ts): Add addProductToOrder type function

--- src/context/db/ctxDao.ts
+++ src/context/db/ctxDao.ts
@@ -1,15 +1,13 @@
-
 // import { PaginationMessageDto } from '../dto/pagination.dto';
 import { UpdateCtxDto } from 'src/bot/dto';
 import { Ctx } from '../entities/ctx.entity';
+import { Product } from '../interface';
 
 export interface ICtxDao {
-
-
   // create(createDoctoDto: CreateDoctorDto): Promise<Doctor>;
 
   findAll(props?: any): Promise<Array<Ctx>>;
-    
+
   // findAllByPagination(paginationMessageDto: PaginationMessageDto): Promise<{ data: Message[]; total: number }>;
 
   // findById(id: string): Promise<Doctor>;
@@ -18,8 +16,13 @@ export interface ICtxDao {
 
   updateCtx(id: string, updateCtxDto: UpdateCtxDto): Promise<Ctx>;
 
-  updateStatusByAppId(appointmentId: string , updateMessageDto: UpdateCtxDto): Promise<Ctx>;
+  updateStatusByAppId(
+    appointmentId: string,
+    updateMessageDto: UpdateCtxDto,
+  ): Promise<Ctx>;
 
+  // se agrega
+  addProductToOrder(id: string, productDto: Product): Promise<Ctx>;
   // findMessageByterm(term: string): Promise<Ctx>;
 
   // remove(id: string): Promise<Doctor>;
