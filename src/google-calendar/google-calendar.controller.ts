9ca0a9e3be1a521380999b07635729a53c0149dd
ferbragon <nando.gon17@gmail.com>
2024-04-24 17:01:23 -0600

    feat(src/google-calendar/google-calendar.controller.ts): Add the GOOGLE_EMAIL_ACCOUNT

--- src/google-calendar/google-calendar.controller.ts
+++ src/google-calendar/google-calendar.controller.ts
@@ -1,37 +1,59 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+} from '@nestjs/common';
 import { GoogleCalendarService } from './google-calendar.service';
 import { calendar } from 'googleapis/build/src/apis/calendar';
-
 
 @Controller('google-calendar')
 export class GoogleCalendarController {
   constructor(private readonly googleCalendarService: GoogleCalendarService) {}
 
-
   @Get('list-upcoming-events')
   async listUpcomingEvents(): Promise<any> {
-    return this.googleCalendarService.listUpcomingEvents('abel3121@gmail.com');
+    return this.googleCalendarService.listUpcomingEvents(
+      process.env.GOOGLE_EMAIL_ACCOUNT,
+    );
   }
 
   @Get('find-available-slots')
   async findAvailableSlots(): Promise<any> {
-    const date = '29/03/2024 15:00:00'
-    return this.googleCalendarService.findAvailableSlots('abel3121@gmail.com', date);
-    }
+    const date = '29/03/2024 15:00:00';
+    return this.googleCalendarService.findAvailableSlots(
+      process.env.GOOGLE_EMAIL_ACCOUNT,
+      date,
+    );
+  }
 
   @Post('create-event')
   async createEvent(@Body() body: any): Promise<any> {
-    return this.googleCalendarService.createEventWithGoogleMeetAndNotifyAttendees('2024-04-08T10:00:00-05:00' ,'2024-04-08T11:00:00-05:00', 'abel3121@gmail.com', 'Test Event');
+    return this.googleCalendarService.createEventWithGoogleMeetAndNotifyAttendees(
+      '2024-04-08T10:00:00-05:00',
+      '2024-04-08T11:00:00-05:00',
+      process.env.GOOGLE_EMAIL_ACCOUNT,
+      'Test Event',
+    );
   }
 
   @Get('find-event-byId')
-  async findEvent(@Query('calendarId') calendarId: string, @Query('eventId') eventId: string): Promise<any> {
-
+  async findEvent(
+    @Query('calendarId') calendarId: string,
+    @Query('eventId') eventId: string,
+  ): Promise<any> {
     return this.googleCalendarService.findEventById(calendarId, eventId);
   }
 
   @Get('cancel-event')
-  async cancelEvent(@Query('calendarId') calendarId: string, @Query('eventId') eventId: string): Promise<any> {
+  async cancelEvent(
+    @Query('calendarId') calendarId: string,
+    @Query('eventId') eventId: string,
+  ): Promise<any> {
     return this.googleCalendarService.findAndCancelEvent(calendarId, eventId);
   }
 }
--- src/google-calendar/google-calendar.service.ts
+++ src/google-calendar/google-calendar.service.ts
@@ -13,23 +13,32 @@ export class GoogleCalendarService {
     this.jwtClient = new JWT({
       email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
       key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-      scopes: ['https://www.googleapis.com/auth/calendar','https://www.googleapis.com/auth/calendar.events'],
+      scopes: [
+        'https://www.googleapis.com/auth/calendar',
+        'https://www.googleapis.com/auth/calendar.events',
+      ],
     });
 
-    this.calendar = google.calendar({ version: 'v3', auth: this.jwtClient as any });
+    this.calendar = google.calendar({
+      version: 'v3',
+      auth: this.jwtClient as any,
+    });
   }
 
   /**
    * Lista los próximos eventos de un calendario específico.
-   * 
+   *
    * @param calendarId ID del calendario de Google para buscar eventos.
    * @param maxResults Número máximo de eventos a devolver.
    */
-  async listUpcomingEvents(calendarId: string, maxResults: number = 10): Promise<calendar_v3.Schema$Event[]> {
+  async listUpcomingEvents(
+    calendarId: string,
+    maxResults: number = 10,
+  ): Promise<calendar_v3.Schema$Event[]> {
     try {
       const response = await this.calendar.events.list({
         calendarId: calendarId,
-        timeMin: (new Date()).toISOString(),
+        timeMin: new Date().toISOString(),
         maxResults: maxResults,
         singleEvents: true,
         orderBy: 'startTime',
@@ -45,20 +54,22 @@ export class GoogleCalendarService {
   /**
    * Encuentra los horarios disponibles en un calendario específico.
    * Esta es una función simplificada, puedes personalizarla según tus necesidades.
-   * 
+   *
    * @param calendarId ID del calendario de Google para buscar eventos disponibles.
    */
 
-  async findAvailableSlots(calendarId: string = 'abel3121@gmail.com', dateString: string): Promise<{day:string,hours: string[] }> {
-    const date = 
-    moment(dateString, 'DD-MM-YYYY').format('YYYY-MM-DD');
+  async findAvailableSlots(
+    calendarId: string = process.env.GOOGLE_EMAIL_ACCOUNT,
+    dateString: string,
+  ): Promise<{ day: string; hours: string[] }> {
+    const date = moment(dateString, 'DD-MM-YYYY').format('YYYY-MM-DD');
     const timeZone = 'America/Lima';
     const format = 'HH:mm';
     const timeMin = moment.tz(date + 'T09:00:00', timeZone);
     const timeMax = moment.tz(date + 'T17:00:00', timeZone);
-    const now = moment.tz(timeZone); 
+    const now = moment.tz(timeZone);
     const todayDate = now.format('YYYY-MM-DD');
-  
+
     try {
       const response = await this.calendar.events.list({
         calendarId: calendarId,
@@ -67,35 +78,52 @@ export class GoogleCalendarService {
         singleEvents: true,
         orderBy: 'startTime',
       });
-  
+
       const events = response.data.items || [];
       const availableSlots = [];
       let currentTime = moment(timeMin);
-  
+
       console.log(`Now: ${now.toString()}`);
-  
+
       while (currentTime < timeMax) {
         const endTime = moment(currentTime).add(60, 'minutes');
-  
-        console.log(`Checking slot: ${currentTime.format(format)} to ${endTime.format(format)}`);
-  
+
+        console.log(
+          `Checking slot: ${currentTime.format(format)} to ${endTime.format(
+            format,
+          )}`,
+        );
+
         // Si estamos consultando el día actual, y la hora final del intervalo ya ha pasado, saltar al siguiente intervalo.
         if (date === todayDate && now.isSameOrAfter(currentTime, 'minute')) {
-          console.log(`Slot ${currentTime.format(format)} to ${endTime.format(format)} has passed.`);
+          console.log(
+            `Slot ${currentTime.format(format)} to ${endTime.format(
+              format,
+            )} has passed.`,
+          );
           currentTime.add(60, 'minutes');
           continue;
         }
-  
-        const isSlotAvailable = events.every(event => {
-          const eventStart = moment.tz(event.start.dateTime || event.start.date, timeZone);
-          const eventEnd = moment.tz(event.end.dateTime || event.end.date, timeZone);
-          return endTime.isSameOrBefore(eventStart) || currentTime.isSameOrAfter(eventEnd);
+
+        const isSlotAvailable = events.every((event) => {
+          const eventStart = moment.tz(
+            event.start.dateTime || event.start.date,
+            timeZone,
+          );
+          const eventEnd = moment.tz(
+            event.end.dateTime || event.end.date,
+            timeZone,
+          );
+          return (
+            endTime.isSameOrBefore(eventStart) ||
+            currentTime.isSameOrAfter(eventEnd)
+          );
         });
-  
+
         if (isSlotAvailable) {
           availableSlots.push(currentTime.format(format));
         }
-  
+
         currentTime.add(60, 'minutes');
       }
 
@@ -114,7 +142,7 @@ export class GoogleCalendarService {
     eventStart: string,
     eventEnd: string,
     summary: string,
-    description?: string
+    description?: string,
   ): Promise<calendar_v3.Schema$Event> {
     const event = {
       summary: summary,
@@ -129,13 +157,13 @@ export class GoogleCalendarService {
       },
       // Añade más propiedades según necesites
     };
-  
+
     try {
       const response = await this.calendar.events.insert({
         calendarId: calendarId,
         requestBody: event,
       });
-  
+
       return response.data;
     } catch (error) {
       console.error('Error al crear evento en Google Calendar:', error);
@@ -146,85 +174,93 @@ export class GoogleCalendarService {
   async createEventWithGoogleMeetAndNotifyAttendees(
     eventStart: string,
     eventEnd: string,
-    calendarId: string = 'abel3121@gmail.com',
-    summary: string='Reunión con el equipo de Ali Bot',
-    description?: string): Promise<calendar_v3.Schema$Event> {
+    calendarId: string = process.env.GOOGLE_EMAIL_ACCOUNT,
+    summary: string = 'Reunión con el equipo de Ali Bot',
+    description?: string,
+  ): Promise<calendar_v3.Schema$Event> {
     try {
-        
-        const event = {
-            summary: summary,
-            description: description,
-            start: {
-                dateTime: eventStart, // Ejemplo: '2024-03-29T09:00:00-05:00'
-                timeZone: 'America/Lima',
-            },
-            end: {
-                dateTime: eventEnd, // Ejemplo: '2024-03-29T10:00:00-05:00'
-                timeZone: 'America/Lima',
-            },
-            conferenceData: {
-              createRequest: { requestId: `hangoutsMeet-${Date.now()}` },
-            },
+      const event = {
+        summary: summary,
+        description: description,
+        start: {
+          dateTime: eventStart, // Ejemplo: '2024-03-29T09:00:00-05:00'
+          timeZone: 'America/Lima',
+        },
+        end: {
+          dateTime: eventEnd, // Ejemplo: '2024-03-29T10:00:00-05:00'
+          timeZone: 'America/Lima',
+        },
+        conferenceData: {
+          createRequest: { requestId: `hangoutsMeet-${Date.now()}` },
+        },
+      };
 
-        };
+      const response = await this.calendar.events.insert({
+        calendarId: calendarId,
+        requestBody: event,
+        conferenceDataVersion: 1, // Importante para que se genere el link de Meet
+        // sendUpdates: 'all' // Importante para enviar notificaciones por email a los asistentes
+      });
 
-        const response = await this.calendar.events.insert({
-            calendarId: calendarId,
-            requestBody: event,
-            conferenceDataVersion: 1, // Importante para que se genere el link de Meet
-            // sendUpdates: 'all' // Importante para enviar notificaciones por email a los asistentes
-        });
-
-        return response.data;
+      return response.data;
     } catch (error) {
-        console.error('Error al crear evento en Google Calendar con Google Meet y notificar a los asistentes:', error);
-        throw new Error('Failed to create event in Google Calendar with Google Meet and notify attendees');
+      console.error(
+        'Error al crear evento en Google Calendar con Google Meet y notificar a los asistentes:',
+        error,
+      );
+      throw new Error(
+        'Failed to create event in Google Calendar with Google Meet and notify attendees',
+      );
     }
   }
 
-  async findEventById(calendarId: string = 'abel3121@gmail.com', eventId: string): Promise<calendar_v3.Schema$Event> {
+  async findEventById(
+    calendarId: string = process.env.GOOGLE_EMAIL_ACCOUNT,
+    eventId: string,
+  ): Promise<calendar_v3.Schema$Event> {
     try {
       const response = await this.calendar.events.get({
         calendarId: calendarId,
         eventId: eventId,
-    });
-    return response.data;    } 
-    catch (error) {
-        console.error('Error finding event by ID:', error.message);
-        throw new Error('Failed to find event by ID');
+      });
+      return response.data;
+    } catch (error) {
+      console.error('Error finding event by ID:', error.message);
+      throw new Error('Failed to find event by ID');
     }
   }
 
-  async updateEventStatusToCancelled(calendarId: string ='abel3121@gmail.com', eventId: string): Promise<string> {
+  async updateEventStatusToCancelled(
+    calendarId: string = process.env.GOOGLE_EMAIL_ACCOUNT,
+    eventId: string,
+  ): Promise<string> {
     try {
-        // Aquí asumimos que "cancelar" un evento significa eliminarlo.
-        // Si prefieres marcarlo de otra manera (ej. cambiar el título o descripción),
-        // deberías usar el método `events.update` en su lugar.
-        await this.calendar.events.delete({ calendarId, eventId });
-        console.log(`Event with ID ${eventId} was cancelled.`);
-        let message = `Event with ID ${eventId} was cancelled.`;
-        return message;
+      // Aquí asumimos que "cancelar" un evento significa eliminarlo.
+      // Si prefieres marcarlo de otra manera (ej. cambiar el título o descripción),
+      // deberías usar el método `events.update` en su lugar.
+      await this.calendar.events.delete({ calendarId, eventId });
+      console.log(`Event with ID ${eventId} was cancelled.`);
+      let message = `Event with ID ${eventId} was cancelled.`;
+      return message;
     } catch (error) {
-        console.error('Error updating event status to cancelled:', error.message);
-        throw new Error('Failed to update event status to cancelled');
+      console.error('Error updating event status to cancelled:', error.message);
+      throw new Error('Failed to update event status to cancelled');
     }
   }
 
-  async findAndCancelEvent(calendarId: string = 'abel3121@gmail.com', eventId: string): Promise<void> {
+  async findAndCancelEvent(
+    calendarId: string = process.env.GOOGLE_EMAIL_ACCOUNT,
+    eventId: string,
+  ): Promise<void> {
     try {
-        const event = await this.findEventById(calendarId, eventId);
-        if (event) {
-            await this.updateEventStatusToCancelled(calendarId, eventId);
-            console.log(`Event with ID ${eventId} was found and cancelled.`);
-        }
+      const event = await this.findEventById(calendarId, eventId);
+      if (event) {
+        await this.updateEventStatusToCancelled(calendarId, eventId);
+        console.log(`Event with ID ${eventId} was found and cancelled.`);
+      }
     } catch (error) {
-        console.error('Error finding and cancelling event:', error.message);
-        throw new Error('Failed to find and cancel event');
+      console.error('Error finding and cancelling event:', error.message);
+      throw new Error('Failed to find and cancel event');
     }
-}
-
-
-
-
-  
+  }
 }
